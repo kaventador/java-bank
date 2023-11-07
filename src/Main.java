e023abf50fdfa8484465b9ed40f758cbc1bc259d
kasra nik <kasranikshabani@yahoo.com>
2023-11-07 10:01:33 +0330

    Bank Sood finall

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("لطفا مبلغ وام را وارد کنید : ");
+        int Vam = scanner.nextInt();
+        System.out.print("باز پرداخت چند  ساله : ");
+        int BazPardakht = scanner.nextInt();
+        System.out.print("در صد سود : ");
+        int Sood = scanner.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int Ghest = BazPardakht*10 ;
+        double SoodM = (double) Sood/(12*100) ;
+        double x = Math.pow((1+SoodM),Ghest);
+
+        double GhestM = ( Vam * SoodM * x ) / x-1;
+        System.out.print( "مبلغ هر قست : ");
+        System.out.print(GhestM);
     }
 }
